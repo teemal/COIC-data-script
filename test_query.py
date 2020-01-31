fdd3b2b2585b10bff5f437b94a9d9b1246a546d8
teemal <taymal1987@gmail.com>
2020-01-31 00:45:34 -0800

    may god have mercy on my soul

--- test_query.py
+++ test_query.py
@@ -18,8 +18,8 @@ GROSS_RENT_PERCENT_INCOME_35_39 = 'B25070_008E'
 GROSS_RENT_PERCENT_INCOME_40_49 = 'B25070_009E'
 GROSS_RENT_PERCENT_INCOME_50_PLUS = 'B25070_010E'
 TOTAL_POPULATION_BURDENED = 'B25070_001E'
-POPULATION_IN_POVERTY = 'B17001_002E'
-TEST = 'B17003_001E'
+HOUSEHOLD_INCOME = 'B19001_00{}E'
+
 
 COMMA = ','
 FOR = '&for='
@@ -35,48 +35,49 @@ JEFFERSON = '031'
 
 # one of many query strings
 FINAL_URL = BASE_URL \
-+ GET + GROSS_RENT_PERCENT_INCOME_50_PLUS \
-+ FOR + COUNTY + "*" \
-+ IN + STATE + OREGON
+    + GET + GROSS_RENT_PERCENT_INCOME_50_PLUS \
+    + FOR + COUNTY + "*" \
+    + IN + STATE + OREGON
 
-r = requests.get(url = FINAL_URL + API_KEY)
+r = requests.get(url=FINAL_URL + API_KEY)
 values = r.json()
 fips_severe_burden = {}
 
 # get population of severe rent burdening by county
-for i in range(1,len(values)):
+for i in range(1, len(values)):
     fips_severe_burden[values[i][2]] = int(values[i][0])
 
 FINAL_URL = BASE_URL \
-+ GET + GROSS_RENT_PERCENT_INCOME_30_34 + COMMA +  GROSS_RENT_PERCENT_INCOME_35_39 + COMMA + GROSS_RENT_PERCENT_INCOME_40_49\
-+ FOR + COUNTY + "*" \
-+ IN + STATE + OREGON
+    + GET + GROSS_RENT_PERCENT_INCOME_30_34 + COMMA + GROSS_RENT_PERCENT_INCOME_35_39 + COMMA + GROSS_RENT_PERCENT_INCOME_40_49\
+    + FOR + COUNTY + "*" \
+    + IN + STATE + OREGON
 
-r = requests.get(url = FINAL_URL + API_KEY)
+r = requests.get(url=FINAL_URL + API_KEY)
 values = r.json()
 fips_rent_burden = {}
 
 # get sum population of rent burdening by county
-for i in range(1,len(values)):
-    fips_rent_burden[values[i][4]] = int(values[i][0]) + int(values[i][1]) + int(values[i][2])
+for i in range(1, len(values)):
+    fips_rent_burden[values[i][4]] = int(
+        values[i][0]) + int(values[i][1]) + int(values[i][2])
 
 FINAL_URL = BASE_URL \
-+ GET + TOTAL_POPULATION_BURDENED \
-+ FOR + COUNTY + "*" \
-+ IN + STATE + OREGON
+    + GET + TOTAL_POPULATION_BURDENED \
+    + FOR + COUNTY + "*" \
+    + IN + STATE + OREGON
 
-r = requests.get(url = FINAL_URL + API_KEY)
+r = requests.get(url=FINAL_URL + API_KEY)
 values = r.json()
 fips_population = {}
 
 # get total population of oregon counties
-for i in range(1,len(values)):
+for i in range(1, len(values)):
     fips_population[values[i][2]] = int(values[i][0])
 
 
-# TODO  POVERTY RATES
+# ========POVERTY=================================
 # FINAL_URL = BASE_URL \
-# + GET + POPULATION_IN_POVERTY \
+# + GET + HOUSEHOLD_INCOME \
 # + FOR + COUNTY + "*" \
 # + IN + STATE + OREGON
 
@@ -85,53 +86,67 @@ for i in range(1,len(values)):
 # pop_in_poverty = {}
 # print(values)
 
-# FINAL_URL = BASE_URL \
-# + GET + TEST \
-# + FOR + COUNTY + "*" \
-# + IN + STATE + OREGON
+NUM_HOUSEHOLD_INCOME_VARIABLES = 17
+for i in range(1, NUM_HOUSEHOLD_INCOME_VARIABLES + 1):
+    #B19001_00 + i + E is a range of income variables in the acs5
+    FINAL_URL = BASE_URL \
+        + GET + ('B19001_00' if i < 10 else 'B19001_0') + str(i) + 'E' \
+        + FOR + COUNTY + "*" \
+        + IN + STATE + OREGON
 
-# r = requests.get(url = FINAL_URL + API_KEY)
+    r = requests.get(url=FINAL_URL + API_KEY)
+    values = r.json()
+    pop_in_poverty = {}
+    print(values)
+
+# FINAL_URL = BASE_URL \
+#     + GET + HOUSEHOLD_INCOME \
+#     + FOR + COUNTY + "*" \
+#     + IN + STATE + OREGON
+
+# r = requests.get(url=FINAL_URL + API_KEY)
 # values = r.json()
 # pop_in_poverty = {}
 # print(values)
+# ===================================================
 
 fips_codes = {
-"001" : "Baker",
-"003" : "Benton",
-"005" : "Clackamas",
-"007" : "Clatsop",
-"009" : "Columbia",
-"011" : "Coos",
-"013" : "Crook",
-"015" : "Curry",
-"017" : "Deschutes",
-"019" : "Douglas",
-"021" : "Gilliam",
-"023" : "Grant",
-"025" : "Harney",
-"027" : "Hood River",
-"029" : "Jackson",
-"031" : "Jefferson",
-"033" : "Josephine",
-"035" : "Klamath",
-"037" : "Lake",
-"039" : "Lane",
-"041" : "Lincoln",
-"043" : "Linn",
-"045" : "Malheur",
-"047" : "Marion",
-"049" : "Morrow",
-"051" : "Multnomah",
-"053" : "Polk",
-"055" : "Sherman",
-"057" : "Tillamook",
-"059" : "Umatilla",
-"061" : "Union",
-"063" : "Wallowa",
-"065" : "Wasco",
-"067" : "Washington",
-"069" : "Wheeler",
-"071" : "Yamhill"
+    "001": "Baker",
+    "003": "Benton",
+    "005": "Clackamas",
+    "007": "Clatsop",
+    "009": "Columbia",
+    "011": "Coos",
+    "013": "Crook",
+    "015": "Curry",
+    "017": "Deschutes",
+    "019": "Douglas",
+    "021": "Gilliam",
+    "023": "Grant",
+    "025": "Harney",
+    "027": "Hood River",
+    "029": "Jackson",
+    "031": "Jefferson",
+    "033": "Josephine",
+    "035": "Klamath",
+    "037": "Lake",
+    "039": "Lane",
+    "041": "Lincoln",
+    "043": "Linn",
+    "045": "Malheur",
+    "047": "Marion",
+    "049": "Morrow",
+    "051": "Multnomah",
+    "053": "Polk",
+    "055": "Sherman",
+    "057": "Tillamook",
+    "059": "Umatilla",
+    "061": "Union",
+    "063": "Wallowa",
+    "065": "Wasco",
+    "067": "Washington",
+    "069": "Wheeler",
+    "071": "Yamhill"
 }
 
 population = {}
@@ -160,36 +175,38 @@ rent_burdened_by_pop = {}
 for key in population:
     if key in rent_burden_total:
         # x100 to shift decimal into percent
-        severe_rent_burdened_by_pop[key] = 100 * (severe_burden_total[key] / population[key])
-        rent_burdened_by_pop[key] = 100 * (rent_burden_total[key] / population[key])
+        severe_rent_burdened_by_pop[key] = 100 * \
+            (severe_burden_total[key] / population[key])
+        rent_burdened_by_pop[key] = 100 * \
+            (rent_burden_total[key] / population[key])
 
-print("severe rent burdened: ")
-print(severe_rent_burdened_by_pop)
-print("")
-print("rent burdened:")
-print(rent_burdened_by_pop)
+# print("severe rent burdened: ")
+# print(severe_rent_burdened_by_pop)
+# print("")
+# print("rent burdened:")
+# print(rent_burdened_by_pop)
 
 workbook = xlsxwriter.Workbook('data.xlsx')
 worksheet = workbook.add_worksheet()
 row = 0
 col = 0
-col_names = ['county', 'population', 'population rent burdened', 'population severly rent burdened']
+col_names = ['county', 'population', 'population rent burdened',
+             'population severly rent burdened']
 for i in col_names:
     worksheet.write(row, col, i)
-    col +=1
+    col += 1
 row = 1
 col = 0
 # there's probably a really slick way to  do this with a lambda
 # but I'm tired and this works
 for i in population:
-    worksheet.write(row,col,i)
+    worksheet.write(row, col, i)
     col += 1
-    worksheet.write(row,col,population[i])
+    worksheet.write(row, col, population[i])
     col += 1
-    worksheet.write(row,col,rent_burdened_by_pop[i])
+    worksheet.write(row, col, rent_burdened_by_pop[i])
     col += 1
-    worksheet.write(row,col,severe_rent_burdened_by_pop[i])
-    row +=1
+    worksheet.write(row, col, severe_rent_burdened_by_pop[i])
+    row += 1
     col = 0
 workbook.close()
-
